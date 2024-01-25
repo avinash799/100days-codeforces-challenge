f77d81344b7de9196c45b13d2e8d451b996e0ce2
AVINASH RAI <76801210+avinash799@users.noreply.github.com>
2024-01-25 15:07:16 +0530

    Add files via upload

---
+++ Jagged swaps.cpp
@@ -0,0 +1,24 @@
+//17th day of 100 days codeforces challenge
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+    ll t;
+    cin>>t;
+   ll minn;
+    while(t--){
+        ll n;
+        cin>>n;
+        ll array[n];
+        for(ll i=0;i<n;i++)
+            cin>>array[i];
+       if(array[0]!=1)
+           cout<<"NO"<<endl;
+       else
+           cout<<"YES"<<endl;
+       
+         
+    }
+    return 0;
+    
+}
